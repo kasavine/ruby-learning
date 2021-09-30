3d54a9b6291a89d6ef2f8294deb13bfdae744d31
kasavine <ivanna.sak@gmail.com>
2021-09-30 16:57:52 -0700

    simple sorting

---
+++ bubble_sort.rb
@@ -0,0 +1,24 @@
+def bubble_sort(array)
+    array_length = array.size
+    return array if array_length <= 1
+
+    loop do
+        swapped = false
+        (array_length-1).times do |i|
+            if array[i] > array[i+1]
+              array[i], array[i+1] = array[i+1], array[i]
+              swapped = true
+            end
+        end
+        if swapped == false
+          break
+        end
+
+    end
+    
+    return array
+      
+end
+    
+puts "Origin: #{[11,5,7,6,15]}"
+puts "Sorted: #{bubble_sort([11,5,7,6,15])}, is it?" 
